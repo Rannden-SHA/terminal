373c1fb9bdbdae4d48387a3a537d742c47d3448b
Rannden-SHA <bd.adrian.gisbert@gmail.com>
2024-06-14 23:02:19 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const greeting = 'agisbert';
+const greeting = 'A D R I A N';
 const intro = 'Welcome, I am a Cybersecurity professional with extensive experience in penetration testing, \nrisk assessment, and network security. \n\nI am Passionate about safeguarding digital landscapes and staying ahead of emerging threats.\nCommitted to continuous learning and applying cutting-edge security measures to ensure data \nintegrity and confidentiality\n';
 
 const user = 'guest';
