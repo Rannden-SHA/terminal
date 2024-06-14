daed3974f69087a8d45493bf89b98a9988f64aad
Rannden-SHA <bd.adrian.gisbert@gmail.com>
2024-06-15 01:19:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ const directories = {
         '* <a target="_blank" href="http://iespolitecnic.com/">IES Politecnic</a> Industrial Automation and Robotics, Palma de Mallorca <yellow>"Higher Degree"</yellow> (2016)',
         '* <a target="_blank" href="https://elorienta.com/macabich/">IES Isidor Macabich</a> Telecommunications, Ibiza <yellow>"Medium Degree"</yellow> (2014)',
     ],
-    "credentials": [
+    "passwords": [
         '',
         '<white>/etc/passwd</white>',
         '',
